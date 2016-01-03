da0e9f4eb948318f271e8c5321aaaacd96111b4a
Christian Specht <christi@nspecht.de>
2016-01-03 15:22:32 +0100

    new project: "Web" -> "Console Application (Package)"
    added .gitignore from https://github.com/github/gitignore/blob/master/VisualStudio.gitignore

---
+++ src/CoreTestVisualStudio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTestVisualStudio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+        }
+    }
+}
