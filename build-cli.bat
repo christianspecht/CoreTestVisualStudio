3774f4dd01c3968dc3cd16c6766b7d4cd91ec93f
Christian Specht <christi@nspecht.de>
2016-01-03 17:13:47 +0100

    create batch files to build
    - build-cli:
    build via dotnet cli
    (runs without errors, but the result doesn't work on a machine with no .NET Core installed)
    
    - build-publishprofile:
    execute the powershell script created by the publish profile
    (doesn't run because of missing parameters)

---
+++ build-cli.bat
@@ -0,0 +1,3 @@
+dotnet publish "%~dp0\src\CoreTestVisualStudio" -c Release -r win7-x64 -o "%~dp0\release\cli"
+
+pause
---
+++ build-publishprofile.bat
@@ -0,0 +1,4 @@
+cd %~dp0\src\CoreTestVisualStudio\Properties\PublishProfiles
+powershell.exe -ExecutionPolicy unrestricted .\test-publish.ps1
+
+pause
